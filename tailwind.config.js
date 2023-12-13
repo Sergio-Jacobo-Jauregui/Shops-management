04457bbe64ac46edee998ff76fb4694d63c778b5
Sergio Jacobo Jauregui <serggiovanni@gmail.com>
2023-12-12 19:01:57 -0500

    ADD: taildwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./shopapp/templates/**/*.{html}",
+            "./shopapp/templates/*.{html}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
